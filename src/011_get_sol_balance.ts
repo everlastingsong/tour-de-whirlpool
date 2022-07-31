1385fade19f2fb0f3ec51db851763505ba8ffdd5
Fuzzy Yeti <fuzzyyeti@github.com>
2022-07-31 12:03:14 -0600

    begin english translation

--- src/011_get_sol_balance.ts
+++ src/011_get_sol_balance.ts
@@ -5,24 +5,35 @@ const RPC_ENDPOINT_URL = "https://api.devnet.solana.com";
 const COMMITMENT = 'confirmed';
 
 async function main() {
-  // Devnet の RPC への要求用のコネクションを作成
+  //LANG:JP Devnet の RPC への要求用のコネクションを作成
+  //LANG:EN Create a connection for sending RPC requests to Devnet
+  //LANG:FR Create a connection for sending RPC requests to Devnet
   const connection = new Connection(RPC_ENDPOINT_URL, COMMITMENT);
 
-  // wallet.json から秘密鍵を読み込み (秘密鍵と公開鍵のペアは Keypair クラスで管理する)
+  //LANG:JP wallet.json から秘密鍵を読み込み (秘密鍵と公開鍵のペアは Keypair クラスで管理する)
+  //LANG:EN Read in the private key from wallet.json (The public and private key pair will be managed using the Keypair class)
   const keypair = Keypair.fromSecretKey(new Uint8Array(secret));
 
-  // 使用する RPC とウォレットの公開鍵を表示
-  // 公開鍵を表示する場合は Base58 形式の文字列で表示する
+  //LANG:JP 使用する RPC とウォレットの公開鍵を表示
+  //LANG:JP 公開鍵を表示する場合は Base58 形式の文字列で表示する
+  //LANG:EN Display the RPC and the wallet's public key
+  //LANG:EN When displaying the public key, use base58 encoding
   console.log("endpoint:", connection.rpcEndpoint);
   console.log("wallet pubkey:", keypair.publicKey.toBase58());
 
-  // SOL 残高取得
-  // Connection クラスの getBalance メソッドを利用する
-  // https://solana-labs.github.io/solana-web3.js/classes/Connection.html#getBalance
+  //LANG:JP SOL 残高取得
+  //LANG:JP Connection クラスの getBalance メソッドを利用する
+  //LANG:JP https://solana-labs.github.io/solana-web3.js/classes/Connection.html#getBalance
+  //LANG:EN Obtain the SOL balance
+  //LANG:EN Use the getBalance method from the Connection class
+  //LANG:EN https://solana-labs.github.io/solana-web3.js/classes/Connection.html#getBalance
   const sol_balance = await connection.getBalance(keypair.publicKey);
 
-  // SOL 残高表示
-  // lamports 単位の整数で内部管理されているため SOL 単位にするには 10^9 で割る (1 SOL = 10^9 lamports)
+  //LANG:JP SOL 残高表示
+  //LANG:JP lamports 単位の整数で内部管理されているため SOL 単位にするには 10^9 で割る (1 SOL = 10^9 lamports)
+  //LANG:EN Display the SOL balance
+  //LANG:EN Since SOL is internally managed as an integer value and denominated in lamports,
+  //LANG:EN divide by 10^9 to obtain a value denominated in SOL.
   console.log("lamports:", sol_balance);
   console.log("SOL:", sol_balance / 10**9);
 }
--- src/012_get_token_balance.ts
+++ src/012_get_token_balance.ts
@@ -7,7 +7,8 @@ const RPC_ENDPOINT_URL = "https://api.devnet.solana.com";
 const COMMITMENT = 'confirmed';
 
 async function main() {
-  // RPC へのコネクション作成、秘密鍵読み込み
+  //LANG:JP RPC へのコネクション作成、秘密鍵読み込み
+  //LANG:EN Initialize a connection to the RPC and read in private key
   const connection = new Connection(RPC_ENDPOINT_URL, COMMITMENT);
   const keypair = Keypair.fromSecretKey(new Uint8Array(secret));
   console.log("endpoint:", connection.rpcEndpoint);
@@ -22,39 +23,57 @@ async function main() {
     "Afn8YB1p4NsoZeS5XJBZ18LTfEy5NFPwN46wapZcBQr6": {name: "devTMAC", decimals: 6},
   };
 
-  // ウォレットの公開鍵からトークンアカウントを取得
-  // https://solana-labs.github.io/solana-web3.js/classes/Connection.html#getTokenAccountsByOwner
-  //
-  // {
-  //   context: { apiVersion: '1.10.24', slot: 140791186 },
-  //   value: [
-  //     { account: [Object], pubkey: [PublicKey] },
-  //     { account: [Object], pubkey: [PublicKey] },
-  //     { account: [Object], pubkey: [PublicKey] },
-  //     { account: [Object], pubkey: [PublicKey] }
-  //   ]
-  // }
+  //LANG:JP ウォレットの公開鍵からトークンアカウントを取得
+  //LANG:JP https://solana-labs.github.io/solana-web3.js/classes/Connection.html#getTokenAccountsByOwner
+  //LANG:JP
+  //LANG:JP {
+  //LANG:JP   context: { apiVersion: '1.10.24', slot: 140791186 },
+  //LANG:JP   value: [
+  //LANG:JP     { account: [Object], pubkey: [PublicKey] },
+  //LANG:JP     { account: [Object], pubkey: [PublicKey] },
+  //LANG:JP     { account: [Object], pubkey: [PublicKey] },
+  //LANG:JP     { account: [Object], pubkey: [PublicKey] }
+  //LANG:JP   ]
+  //LANG:JP }
+  //LANG:EN Obtain the token accounts from the wallet's public key
+  //LANG:EN https://solana-labs.github.io/solana-web3.js/classes/Connection.html#getTokenAccountsByOwner
+  //LANG:EN
+  //LANG:EN {
+  //LANG:EN   context: { apiVersion: '1.10.24', slot: 140791186 },
+  //LANG:EN   value: [
+  //LANG:EN     { account: [Object], pubkey: [PublicKey] },
+  //LANG:EN     { account: [Object], pubkey: [PublicKey] },
+  //LANG:EN     { account: [Object], pubkey: [PublicKey] },
+  //LANG:EN     { account: [Object], pubkey: [PublicKey] }
+  //LANG:EN   ]
+  //LANG:EN }
   const accounts = await connection.getTokenAccountsByOwner(
     keypair.publicKey,
     { programId: TOKEN_PROGRAM_ID }
   );
   console.log("getTokenAccountsByOwner:", accounts);
 
-  // トークンアカウントのデータをデコード
+  //LANG:JP トークンアカウントのデータをデコード
+  //LANG:EN Deserialize token account data
   for (let i=0; i<accounts.value.length; i++) {
     const value = accounts.value[i];
 
-    // デコード
+    //LANG:JP デコード
+    //LANG:EN Deserialize
     const parsed_token_account = TokenUtil.deserializeTokenAccount(value.account.data);
-    // mint アドレスからどのトークンのトークンアカウントか特定
+    //LANG:JP mint アドレスからどのトークンのトークンアカウントか特定
+    //LANG:EN Use the mint address to determine which token account is for which token
     const mint = parsed_token_account.mint;
     const token_def = token_defs[mint.toBase58()];
-    // devToken 以外は無視
+    //LANG:JP devToken 以外は無視
+    //LANG:EN Ignore non-devToken accounts
     if ( token_def === undefined ) continue;
 
-    // 残高は amount
+    //LANG:JP 残高は amount
+    //LANG:EN The balance is "amount"
     const amount = parsed_token_account.amount;
-    // 残高は整数で管理されているため、UI 表示用に変換
+    //LANG:JP 残高は整数で管理されているため、UI 表示用に変換
+    //LANG:EN The balance is managed as an integer value, so it must be converted for UI display
     const ui_amount = DecimalUtil.fromU64(amount, token_def.decimals);
 
     console.log(
