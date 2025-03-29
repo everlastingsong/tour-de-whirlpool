c2d2173f78fbbe1c70ae97636bf7601bf3fba76e
yugure <109891005+yugure-orca@users.noreply.github.com>
2025-03-30 03:49:42 +0900

    tweak code

--- src_i18n/112_create_splashpool.ts
+++ src_i18n/112_create_splashpool.ts
@@ -8,8 +8,8 @@ import {
 } from "@orca-so/whirlpools-sdk";
 import { Keypair, PublicKey } from "@solana/web3.js";
 import Decimal from "decimal.js";
-import secret from "../wallet.json";
 import { createMint } from "@solana/spl-token";
+
 // UNIX/Linux/Mac
 // bash$ export ANCHOR_PROVIDER_URL=https://api.devnet.solana.com
 // bash$ export ANCHOR_WALLET=wallet.json
@@ -28,58 +28,61 @@ import { createMint } from "@solana/spl-token";
 
 // LANG:EN These are the addresses of Orca owned Whirlpool Configs.
 // LANG:EN For more details, see https://dev.orca.so/Architecture%20Overview/Account%20Architecture
-const ORCA_WHIRLPOOLS_CONFIG_MAINNET = new PublicKey(
-  "2LecshUwdy9xi7meFgHtFJQNSKk4KdTrcpvaB56dP2NQ"
-);
-const ORCA_WHIRLPOOLS_CONFIG_DEVNET = new PublicKey(
-  "FcrweFY1G9HJAHG5inkGB6pKg1HZ6x9UC2WioAfWrGkR"
-);
+const DEVNET_WHIRLPOOLS_CONFIG = new PublicKey("FcrweFY1G9HJAHG5inkGB6pKg1HZ6x9UC2WioAfWrGkR");
 
 async function main() {
+  //LANG:JP WhirlpoolClient 作成
+  //LANG:EN Create WhirlpoolClient
   const provider = AnchorProvider.env();
-  const ctx = WhirlpoolContext.withProvider(
-    provider,
-    ORCA_WHIRLPOOL_PROGRAM_ID
-  );
+  const ctx = WhirlpoolContext.withProvider(provider, ORCA_WHIRLPOOL_PROGRAM_ID);
   const client = buildWhirlpoolClient(ctx);
-  const signer = Keypair.fromSecretKey(new Uint8Array(secret));
+
+  console.log("endpoint:", ctx.connection.rpcEndpoint);
+  console.log("wallet pubkey:", ctx.wallet.publicKey.toBase58());
 
   // LANG:EN Create new token mints. Note that the in a more realistic scenario,
   // LANG:EN the mints are generated beforehand.
-  const decimalsA = 6;
-  const decimalsB = 9;
-  const mintPublicKeyA = await createMint(
-    ctx.connection,
-    signer,
-    ctx.wallet.publicKey,
-    null,
-    decimalsA
-  );
-  const mintPublicKeyB = await createMint(
-    ctx.connection,
-    signer,
-    ctx.wallet.publicKey,
-    null,
-    decimalsB
-  );
+  const walletKeypair = (ctx.wallet as unknown as { payer: Keypair }).payer;
+  const newTokenPubkeys = await Promise.all([
+    createMint(
+        ctx.connection,
+        walletKeypair,
+        ctx.wallet.publicKey, // mint authority
+        null, // freeze authority
+        9, // decimals
+      ),
+    createMint(
+        ctx.connection,
+        walletKeypair,
+        ctx.wallet.publicKey, // mint authority
+        null, // freeze authority
+        6, // decimals
+      ),
+  ]);
 
   // LANG:EN Token A and Token B Mint has to be cardinally ordered
-  const [mintAddressA, mintAddressB] = PoolUtil.orderMints(
-    mintPublicKeyA.toString(),
-    mintPublicKeyB.toString()
-  );
+  // LANG:EN For example, SOL/USDC can be created, but USDC/SOL cannot be created
+  const [tokenAddressA, tokenAddressB] = PoolUtil.orderMints(newTokenPubkeys[0], newTokenPubkeys[1]);
+
+  // LANG:EN Fetch token mint infos
+  const tokenA = await ctx.fetcher.getMintInfo(tokenAddressA);
+  const tokenB = await ctx.fetcher.getMintInfo(tokenAddressB);
+  const decimalsA = tokenA.decimals;
+  const decimalsB = tokenB.decimals;
+  console.log("tokenA:", tokenAddressA.toString(), "decimals:", decimalsA);
+  console.log("tokenB:", tokenAddressB.toString(), "decimals:", decimalsB);
 
   // LANG:EN Set the price of token A in terms of token B
   const initialPrice = new Decimal(0.01);
 
+  // LANG:EN Create a new pool
   const { poolKey, tx } = await client.createSplashPool(
-    ORCA_WHIRLPOOLS_CONFIG_DEVNET,
-    mintAddressA,
-    mintAddressB,
+    DEVNET_WHIRLPOOLS_CONFIG,
+    tokenAddressA,
+    tokenAddressB,
     initialPrice,
     ctx.wallet.publicKey
   );
-
   const txId = await tx.buildAndExecute();
 
   // LANG:EN Fetch pool data to verify the initial price and tick
@@ -93,9 +96,14 @@ async function main() {
   const poolInitialTick = poolData.tickCurrentIndex;
 
   console.log("txId:", txId);
-  console.log("poolKey:", poolKey.toBase58());
-  console.log("initialPrice:", poolInitialPrice);
-  console.log("initialTick:", poolInitialTick);
+  console.log(
+    "poolKey:", poolKey.toBase58(),
+    "\n  tokenA:", poolData.tokenMintA.toBase58(),
+    "\n  tokenB:", poolData.tokenMintB.toBase58(),
+    "\n  tickSpacing:", poolData.tickSpacing,
+    "\n  initialPrice:", poolInitialPrice,
+    "\n  initialTick:", poolInitialTick
+  );
 }
 
 main();
